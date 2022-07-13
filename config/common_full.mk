833b266e41937c5504b76c166986e3d9e4549648
Omkar Chandorkar <gotenksIN@aospa.co>
2022-07-12 23:24:31 -0400

    overlay: set raven TP1A fingerprint
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Change-Id: Ib19b30da1c49cc3442240ea85a8335a2a6cf3e22

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,6 +3,10 @@ $(call inherit-product, vendor/derp/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
+# Overlays
+PRODUCT_PACKAGES += \
+    PixelImitationOverlay
+
 # Fonts
 PRODUCT_PACKAGES += \
     fonts_customization.xml
