1eee03224b270d780d0e24d0f53071926dd45390
NurKeinNeid <mralexman3000@gmail.com>
2022-08-27 19:35:51 +0200

    Revert "Revert "derp: Include livedisplay permissions""
    This reverts commit e30b362aaf1220b6429a257063944c089288e0c3.

--- config/common.mk
+++ config/common.mk
@@ -84,6 +84,10 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_COPY_FILES += \
     vendor/derp/prebuilt/common/etc/permissions/default-permissions-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions-product.xml
 
+# Livedisplay
+PRODUCT_COPY_FILES += \
+    vendor/derp/prebuilt/common/etc/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.
